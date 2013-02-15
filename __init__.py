4646ada918aaab377e3b8d17cb0236407ddb9b6c
Brett Calcott <brett.calcott@gmail.com>
2013-02-15 14:50:48 +1100

    get rid of parameters

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from simulation import Simulation, Interrupt
 from config import Configuration
-from parameters import Parameters
 from context import Context
 from plugin import ExperimentPlugin, TreatmentPlugin, ReplicatePlugin
